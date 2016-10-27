fd6d8dd9ab4f813280039177642d10e55bc30985
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-27 16:01:21 +0700

    get first sheet, filter empty Keyword

--- resources/views/excel/load.blade.php
+++ resources/views/excel/load.blade.php
@@ -23,18 +23,23 @@
                         <input type="file" name="excel_default_file" class="form-control">
                     </div>
 
-                    <small class="text-muted">Upload your boyfriend_default file, a fallback answer when keyword not found</small>
+                    <small class="text-muted">Upload your boyfriend_default file, a fallback answer when keyword not
+                        found
+                    </small>
                 </div>
 
-                <pre id="wbJsonPre"></pre>
+                <pre id="wbObjPre"></pre>
 
                 <button id="btnLoad" class="btn btn-info">Load</button>
             </form>
         </div>
     </div>
     <style>
-        #wbJsonPre{
+        #wbObjPre {
             white-space: normal;
+            max-height: 200px;
+            overflow-y: scroll;
+            overflow-x: hidden;
         }
     </style>
 @endsection
@@ -46,35 +51,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2014-11-29/FileSaver.min.js"></script>
     <script src="{{ url('js/parse-regex.js') }}"></script>
     <script>
-        /**
-         * Created by hoanganh25991 on 29/09/16.
-         */
-//        let html = '<p><a href="#download" id="download">Download<a> sample <strong>excel-file</strong></p>';
-//        let flashDiv = $('.alert');
         console.log(flashDiv);
-//        flashDiv.html(html);
-//        flashDiv.attr('class', 'alert alert-info');
-
-        let downloadBtn = $('#download');
-        downloadBtn.on('click', function(e){
-            e.preventDefault();
-            console.log('click');
-            let url = "{{ url('load-file?file_name=rooms.xlsx') }}";
-            let win = window.open(url, '_blank');
-            win.focus();
-        });
-
+        //for main excel file
         let excel_file = $('input[name="excel_file"]');
-
-        let excel_default_file = $('input[name="excel_default_file"]');
-
         let excelParsed = $('input[name="excel_parsed"]');
-
+        //for default file
+        let excel_default_file = $('input[name="excel_default_file"]');
         let excelDefaultParsed = $('input[name="excel_default_parsed"]');
 
         let form = $('form#loadChatbox');
-
-        console.log(excel_file);
 
         excel_file.on('change', handleFile);
 
@@ -85,57 +70,69 @@
             let f1 = files[0];
             let fileReader = new FileReader();
             fileReader.readAsBinaryString(f1);
+
             fileReader.onload = function(e){
                 let data = e.target.result;
-                //noinspection JSUnresolvedVariable
                 let wb = XLSX.read(data, {type: 'binary'});
 
-                //@warn: Sheet1 : hard-code
-                //detect through sheet name array
-                let wbJson = XLSX.utils.sheet_to_row_object_array(wb.Sheets['Sheet1']);
-                wbJson.forEach(function(val){
-                    //handle val['Keyword']
-                    //PARSE IT into php preg_match
-                    let keyword = val['Keyword'];
-                    let pattern = parseKeyword(keyword);
-                    val['Keyword'] = pattern;
-                })
-                ;
-                // console.log(wbJson);
-                let wbJsonPre = $('#wbJsonPre');
-                let wbJsonStr = JSON.stringify(wbJson);
-//                wbJsonPre.html(`<pre>${wbJsonStr}</pre>`);
-                wbJsonPre.text(wbJsonStr);
-                excelParsed.val(wbJsonStr);
+                let sheetNames = wb.SheetNames;
+                //wb always has a default sheet
+                let firstSheet = wb.Sheets[sheetNames[0]];
+                let wbObj = XLSX.utils.sheet_to_row_object_array(firstSheet);
+
+                wbObj = wbObj
+                        .filter(function(val){
+                            //remove out where Keyword
+                            return val['Keyword'];
+                        })
+                        .map(function(val){
+                            //handle val['Keyword']
+                            //PARSE IT into php preg_match
+                            let keyword = val['Keyword'];
+                            let pattern = parseKeyword(keyword);
+                            val['Keyword'] = pattern;
+                            return val;
+                        });
+                console.log('wbObj', wbObj);
+                // console.log(wbObj);
+                let wbObjPre = $('#wbObjPre');console.log(wbObjPre);
+                let wbObjStr = JSON.stringify(wbObj);
+                // wbObjPre.html(`<pre>${wbObjStr}</pre>`);
+                wbObjPre.text(wbObjStr);
+                excelParsed.val(wbObjStr);
             }
-        };
+        }
 
         function handleDefaultFile(e){
             let files = e.target.files;
             let f1 = files[0];
             let fileReader = new FileReader();
             fileReader.readAsBinaryString(f1);
+            
             fileReader.onload = function(e){
                 let data = e.target.result;
-                //noinspection JSUnresolvedVariable
                 let wb = XLSX.read(data, {type: 'binary'});
-                let wbSheet1 = wb.Sheets['Sheet1'];
+
+                let sheetNames = wb.SheetNames;
+                //wb always has a default sheet
+                let wbSheet1 = wb.Sheets[sheetNames[0]];
+                
                 let wbSheet1Ref = wbSheet1['!ref'];
                 let refRange = XLSX.utils.decode_range(wbSheet1Ref);
-                let wbJson = [];
-                for(var R = refRange.s.r; R <= refRange.e.r; ++R) {
-                    for(var C = refRange.s.c; C <= refRange.e.c; ++C) {
-                        var cell_address = {c:C, r:R};
-                        var cellVal = wbSheet1[XLSX.utils.encode_cell(cell_address)].v;
-                        wbJson.push(cellVal);
-                    }
+                
+                let wbObj = [];
+                for(var R = refRange.s.r; R <= refRange.e.r; ++R){
+                    //only read the first col refRange.s.c
+                    var cellAddress = {c: refRange.s.c, r: R};
+                    var cellVal = wbSheet1[XLSX.utils.encode_cell(cellAddress)].v;
+                    wbObj.push(cellVal);
                 }
 
-                let wbJsonPre = $('#wbJsonPre');
-                let wbJsonStr = JSON.stringify(wbJson);
-//                wbJsonPre.html(`<pre>${wbJsonStr}</pre>`);
-                wbJsonPre.text(wbJsonStr);
-                excelDefaultParsed.val(wbJsonStr);
+                let wbObjPre = $('#wbObjPre');
+                let wbObjStr = JSON.stringify(wbObj);
+//                wbObjPre.html(`<pre>${wbObjStr}</pre>`);
+                wbObjPre.text(wbObjStr);
+                excelDefaultParsed.val(wbObjStr);
             }
         }
 
@@ -149,6 +146,7 @@
                 flash(`Please upload <strong>both boyfriend & boyfriend_default file</strong>`, 'warning');
                 return;
             }
+            
             form.submit();
         });
     </script>
