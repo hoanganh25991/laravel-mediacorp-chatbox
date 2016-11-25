2b559c269ad0619d8fdf9b1f71ef940422b484fc
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-11-25 15:52:54 +0700

    draft refactor on Conversation

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Conversation;
-use DB;
 use Validator;
-//use \PDO;
 use App\Traits\ApiResponse;
 use App\Traits\ApiUtils;
 
@@ -15,153 +13,147 @@ class ConversationController extends Controller {
     use ApiUtils;
 
     const NO_ANSWER = "I hear you, but no anwser";
+    const MIS_CONTEXT = "Sorry, i miss your context";
     
     public function script(Request $req){
-        //only get out main boyfriend chatbox
-        //default is for fallback
+        /**
+         * 1. Only get out main boyfriend chatbox
+         * 2. Boyfriend default is for fallback
+         */
         $conversations = Conversation::where('name', 'not like', '%default%')->get();
 
+        /**
+         * Handle GET
+         */
         if($req->method() == 'GET'){
             return view('conversation.script')->with(compact('conversations'));
         }
 
-        if($req->method() == 'POST'){
-            $validator = Validator::make($req->all(), [
-                'chatbox_name' => 'required',
-                'user_text' => 'required'
-            ]);
-            
-            if($validator->fails()){
-                return $this->res($req->all(), $validator->messages(), 422);
-            }
-            
-            $chatboxName = $req->get('chatbox_name');
-            //find out conversation base on chatboxName
-            //store into collection
-            $conversation = $conversations->where('name', $chatboxName)->first();
-//            $conversation = $conversations->where('name', 'asdfasdf')->first();
-            // Check no $conversation added
-            if(empty($conversation)){
-                $answer = 'I\'m sorry. But, no chatbox added. Ask admin for help.';
-                return response(['response' => $answer], 200, ['Content-Type' => 'application/json']);
-            }
+        /**
+         * Handle NOT GET, implicit for POST
+         */
+        $validator = Validator::make($req->all(), [
+            'chatbox_name' => 'required',
+            'user_text' => 'required'
+        ]);
 
-            $conversation = json_decode($conversation->content, true);
-            $conversation = collect($conversation);
-
-            //query on $conversation, get out ANSWER
-            $userText = $req->get('user_text');
-//            $userReplyOrigin = $req->get('user_text_origin');
-
-            /**
-             * Check user only using emoji|emoticon
-             */
-//            $removeEmojiUserText = $this->removeEmoji($userText);
-            $removeEmojiUserText = $this->removeEmoji($userText);
-            if(empty($this->removeSpace($removeEmojiUserText))){
-                return $this->res(['response' => self::NO_ANSWER]);
-            }
-//            $userReply = "{$userReply} {$userReplyOrigin}";
-            //add space into userReply, bcs day? considerd as whole ONE
-            //which dif from 'day ?' considered as 'day' and '?'
-            //@info php preg_match can solve 'how are you?'
-            //no need to modify here
-//            $arr = explode('?', $userReply);
-//            $userReply = implode(' ?', $arr);
-            $userText = $this->removeSomeSC($userText);
-            $userReplySingularForm = $this->transformWordsToSingular($userText);
-//            $userReplyOrigin = preg_replace("/\.|\.\.|!/", '', $userReplyOrigin);
-            //find out matched answer in conversation
-            $answers = $conversation->filter(function($val) use($userText){
-                $pattern = $val['Keyword'];
-
-                return preg_match($pattern, $userText);
-            });
-
-            $answers2 = $conversation->filter(function($val) use($userReplySingularForm){
-                $pattern = $val['Keyword'];
-
-                return preg_match($pattern, $userReplySingularForm);
-            });
-            //only get the values, store as array, filter make HOLE in array
-            //array like object 2=>'asdfasd', 4=>'asdfasdf', bcs 0,1,3 gone
-            $answers = $answers->values();
-            $answers2 = $answers2->values();
-
-            $answers = $answers->merge($answers2);
-
-            $answer = '';
-
-            /**
-             * may be more then 1 $answers
-             * just choose the first one $answer[0]
-             */
-            if($answers->count() > 0){
-//                $NO_ANSWER = "I hear you, but no anwser";
-                try{
-                    if($answers[0]['Response 1'] == '-'
-                        && $answers[0]['Response 2'] == '-'
-                        && $answers[0]['Response 3'] == '-'){
-//                        $answer = $NO_ANSWER;
-                        $answer = self::NO_ANSWER;
-                    }
-                }catch(\Exception $e){
-                    $answer = '';
-                }
-
-
-                if(empty($answer)){
-
-                    /**
-                     * SET DEFAULT AT ONE
-                     */
-                    if(!empty($answers[0]['Response 1'])){
-                        $answer = $answers[0]['Response 1'];
-                    }
-
-                    //Play random case
-                    //only get the higher response  $answers[0]
-                    //random one
-                    $x = random_int(1, 3);
-                    $responseX = "Response {$x}";
-                    //some answer return as -
-                    //what the HECK @@, fall back to the 'Response 1'
-                    if(!empty($answers[0][$responseX]) && $answers[0][$responseX] != '-'){
-                        $answer = $answers[0][$responseX];
-                    }
-                }
-            }
-            //if NO answer found, load random one in default
-//            if(!($answers->count() > 0)){
-            if(empty($answer)){
-                $tmp = explode('.', $chatboxName);
-                $chatboxNameWithoutExt = $tmp[0];
-//                $pattern = "(^({$chatboxNameWithoutExt}).*default)";
-                $pattern = "%{$chatboxNameWithoutExt}%default%";
-                //look for default conversation
-//                $conversation = Conversation::where('name', 'regexp', $pattern)->first();
-                // 'expr' to work with sqlite
-//                $conversation = Conversation::where('name', 'expr', $pattern)->first();
-//                $conversation = Conversation::where('name', '=', 'boyfriend1_default.xlsx')->first();
-
-
-                $conversation = Conversation::where('name', 'like', $pattern)->first();
-
-                if($conversation && $conversation->count() > 0){
-                    $conversation = json_decode($conversation->content, true);
-                    $conversation = collect($conversation);
-//                    $answer = $conversation[random_int(0, $conversation->count() - 1)];
-                    if($conversation->count() > 0){
-                        $answer = $conversation->random(1);
-                    }
-                }
-            }
-
-            // Still empty, notify her
-            if(empty($answer))
-                $answer = 'Sorry, i miss your context';
-
-            return response(['response' => $answer], 200, ['Content-Type' => 'application/json']);
+        if($validator->fails()){
+            return $this->res($req->all(), $validator->messages(), 422);
         }
+
+        $chatboxName = $req->get('chatbox_name');
+        $conversation = $conversations->where('name', $chatboxName)->first();
+        if(empty($conversation)){
+            $msg = 'I\'m sorry. But, no chatbox added. Ask admin for help.';
+            return $this->res($req->all(), $msg, 422);
+        }
+        /**
+         * Extract conversation on content
+         */
+        $conversation = json_decode($conversation->content, true);
+        // Convert to collection of laravel for easy manipulate
+        $conversation = collect($conversation);
+
+        $userText = $req->get('user_text');
+        /**
+         * If user only reply emoji|emoticon
+         * "NO RESPONSE"
+         */
+        $removeEmojiUserText = $this->removeEmoji($userText);
+        if(empty($this->removeSpace($removeEmojiUserText))){
+            return $this->res(['response' => self::NO_ANSWER]);
+        }
+        // Remove some special character
+        $userText = $this->removeSomeSC($userText);
+        // Support singular form, when user type in plural
+        $userTextSingularForm = $this->transformWordsToSingular($userText);
+
+        // Find out answer for origin user_text
+        $answersUserText =
+            $conversation
+                ->filter(function($val) use($userText){
+                    $pattern = $val['Keyword'];
+                    return preg_match($pattern, $userText);
+                })
+                // After filter, array has "HOLE", just get values as array
+                ->values();
+
+        $answersUserTextSingularForm = 
+            $conversation
+                ->filter(function($val) use($userTextSingularForm){
+                    $pattern = $val['Keyword'];
+                    return preg_match($pattern, $userTextSingularForm);
+                })
+                // After filter, array has "HOLE", just get values as array
+                ->values();
+
+        $answers = $answersUserText->merge($answersUserTextSingularForm);
+
+        $answer = $this->getMostRelevanceAnswer($answers);
+
+        /**
+         * In case, no answer found out
+         * Try on fallback of boyfriend default
+         */
+        if(empty($answer)){
+            $tmp = explode('.', $chatboxName);
+            $chatboxNameWithoutExt = $tmp[0];
+            $pattern = "%{$chatboxNameWithoutExt}%default%";
+            $defaultConversation = Conversation::where('name', 'like', $pattern)->first();
+
+            if(!empty($defaultConversation)){
+                // Parse out content
+                $defaultConversation = json_decode($defaultConversation->content, true);
+                // Use laravel collection
+                $defaultConversation = collect($defaultConversation);
+
+                if($defaultConversation->count() > 0){
+                    $answer = $defaultConversation->random(1);
+                }
+            }
+        }
+
+        /**
+         * Seldomly, but still empty, notify out
+         */
+        if(empty($answer))
+            $answer = self::MIS_CONTEXT;
+
+        return response(['response' => $answer], 200, ['Content-Type' => 'application/json']);
+    }
+
+    private function getMostRelevanceAnswer($answers){
+        $answer = "";
+
+        if($answers->count() > 0){
+            /**
+             * Just choose the first one $answer[0] as most relavanace
+             */
+            $answer1 = $answers[0];
+            /**
+             * 1. Response 1, 2, 3 may not exist!!! Ask the script writer why @@
+             */
+            try{
+                /**
+                 * When res 1, 2, 3 == '-', "NO_ANSWER"
+                 */
+                if($answer1['Response 1'] == '-'
+                    && $answer1['Response 2'] == '-'
+                    && $answer1['Response 3'] == '-')
+                {
+                    return self::NO_ANSWER;
+                }
+
+                // Play random
+                $x = random_int(1, 3);
+                $responseX = "Response {$x}";
+
+                return $answer1[$responseX];
+            }catch(\Exception $e){
+                return isset($answer1['Response 1']) ? $answer1['Response 1'] : "";
+            }
+        }
+
+        return $answer;
     }
 }
